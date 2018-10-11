f6d6f807ceae225b883213a2ab9f0c706acec9d3
Cameron Joseph Brown <camquarter2015@gmail.com>
2018-10-10 23:19:14 -0600

    Initial Commit

---
+++ Lab 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab 2
+//
+//  Created by Cam on 10/10/18.
+//  Copyright © 2018 Cameron Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
